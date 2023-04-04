5bab160de97646385718f300c900a620b6a12b6b
Lou Foster <lawuana.foster@att.net>
2023-04-04 02:33:34 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -65,3 +65,35 @@ data.forEach((dataRow) => {
 //      Appended a row to the HTML table
 //      Added each value from the object into a cell
 // For example, this is the very first object in our array:
+
+
+function handleClick() {
+      // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // pseudocode practice
+//if (a date is entered) {
+//    Filter the default data to show only the date entered
+//};
+
+//  // Check to see if a date was entered and filter the data using that date.
+if (date) {
+    // Applying a filter method that will match the datetime value to the filtered date value.
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+buildTable(tableData);
